284a0ba92ff3025fe7a0b5f1334d6a0f8fabaa7d
Ian Chen <ianchen06@gmail.com>
2024-05-17 13:03:24 -0700

    add cosmosdb

--- function_app.py
+++ function_app.py
@@ -1,10 +1,15 @@
 import os
+import json
+import uuid
+
 import azure.functions as func
 import logging
 from azure.cosmos import CosmosClient
 
 COSMOSDB_URL = os.getenv("COSMOSDB_URL")
 COSMOSDB_KEY = os.getenv("COSMOSDB_KEY")
+COSMOSDB_DATABASE = os.getenv("COSMOSDB_DATABASE")
+COSMOSDB_CONTAINER = os.getenv("COSMOSDB_CONTAINER")
 
 app = func.FunctionApp()
 client = CosmosClient(COSMOSDB_URL, credential=COSMOSDB_KEY)
@@ -20,4 +25,13 @@ def eventhub_trigger1(azeventhub: func.EventHubEvent):
         "Python EventHub trigger processed an event: %s",
         azeventhub.get_body().decode("utf-8"),
     )
-    
+
+    msg = json.loads(azeventhub.get_body().decode("utf-8"))
+    msg["id"] = str(uuid.uuid4())
+    logging.info(f"Message: {msg.get('device_id')}")
+
+    (
+        client.get_database_client(COSMOSDB_DATABASE)
+        .get_container_client(COSMOSDB_CONTAINER)
+        .upsert_item(msg)
+    )
